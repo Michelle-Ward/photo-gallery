799206092db425f34511260eacd6a7de5ca68665
Adam Brent <adam.brent@gmail.com>
2021-01-15 10:10:50 -0800

    Adds return statement to generateData.

--- server/database/seed.js
+++ server/database/seed.js
@@ -1,7 +1,22 @@
-const { buildProperty, buildPhotoList } = require('../../utils/datafaker.js');
+const { buildProperty, buildPhotoList } = require('./datafaker.js');
 
-let photoList = buildPhotoList(false, photos = 40);
-console.log(photoList);
+const generateData = () => {
+  let data = [];
+  for (let i = 0; i < 100; i++) {
+    let property = buildProperty();
+    property.photos = buildPhotoList(property.apartment, photos = 40);
+    data = data.push(property);
+  }
+  return data;
+};
 
-
-
+// Generate data
+// Save to json
+// Create db if not exists
+// Connect to db
+// Load Models
+// Sync db
+// Create property table records for each property
+  // Create photo table records for each photo
+    // Upload associated photos to S3
+// Close connection
