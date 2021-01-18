e2bd5d29cd35b718b2d7dc398b1da72d6301c30b
Adam Brent <adam.brent@gmail.com>
2021-01-18 01:00:23 -0800

    Adds format to db programatically as part of seed script.

--- server/database/datafaker.js
+++ server/database/datafaker.js
@@ -57,7 +57,8 @@ var buildPhotoList = (apartment, photos = 1) => {
   apartment === true ? type = 'apartment' : type = 'house';
   for (let i = 1; i <= photos; i++) {
     let link = `https://abphotos.s3-us-west-2.amazonaws.com/AXlia/image${i}.jpg`;
-    photoList.push({link});
+    let format = link.substring(link.lastIndexOf('.') + 1, link.length);
+    photoList.push({link, format});
   }
   return photoList;
 };
