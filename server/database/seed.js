c795bdfdfbfa11cccf0a21f4839505ecafe12bf4
Adam Brent <adam.brent@gmail.com>
2021-01-18 00:39:52 -0800

    Closes database at end of record creation in seeding script. Code cleanup.

--- server/database/seed.js
+++ server/database/seed.js
@@ -2,16 +2,11 @@ const { buildProperty, buildPhotoList } = require('./dataFaker.js');
 const { db, initializeDB } = require('./index.js');
 const { property, photo } = require('./models.js');
 
-property.photo = property.hasMany(photo, {
-  foreignKey: 'propertyId'
-});
-photo.property = photo.belongsTo(property);
-
 const generateData = () => {
   let data = [];
   for (let i = 0; i < 100; i++) {
     let propertyEntry = buildProperty();
-    propertyEntry.photos = buildPhotoList(propertyEntry.apartment, photos = 40);
+    propertyEntry.photos = buildPhotoList(propertyEntry.apartment, photos = 34);
     data.push(propertyEntry);
   }
   return data;
@@ -26,7 +21,7 @@ const seed = async () => {
         association: property.photo,
       }]
     })
-    .then(res => {});
+    .then(res => {db.close();});
     console.log('Records created!');
   } catch (err) {
     console.log('Error creating records:', err);
@@ -35,13 +30,3 @@ const seed = async () => {
 
 seed();
 
-// Generate data
-// Save to json
-// Create db if not exists
-// Connect to db
-// Load Models
-// Sync db
-// Create property table records for each property
-  // Create photo table records for each photo
-  // https://abphotos.s3-us-west-2.amazonaws.com/AXlia/image21.jpg
-// Close connection
